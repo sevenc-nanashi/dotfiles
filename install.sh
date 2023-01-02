bcfd3832239eba927e521924d5a5a8530b1bcae6
Nanashi <sevenc7c@sevenc7c.com>
2023-01-02 22:06:46 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ git clone https://github.com/sevenc-nanashi/dotfiles ~/dotfiles
 cd ~/dotfiles
 curl -fLo ~/.local/share/nvim/site/pack/jetpack/opt/vim-jetpack/plugin/jetpack.vim --create-dirs https://raw.githubusercontent.com/tani/vim-jetpack/master/plugin/jetpack.vim
 
-ln -s ./neovim/ ~/.config/nvim
-ln -s ./.gitconfig ~/.gitconfig
-ln -s ./.gitignore_global ~/.gitignore_global 
-ln -s ./starship.toml ~/.config/starship.toml
+ln -s ~/dotfiles/neovim/ ~/.config/nvim
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/dotfiles/.gitignore_global ~/.gitignore_global 
+ln -s ~/dotfiles/starship.toml ~/.config/starship.toml
