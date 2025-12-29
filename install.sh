60cc3d2cd82d91d5fea1acddf61f05206157431f
sevenc-nanashi <sevenc7c@sevenc7c.com>
2025-12-29 18:47:34 +0900

    feat: miseに全部賭ける

--- install.sh
+++ install.sh
@@ -17,14 +17,7 @@ sudo apt install -y unzip curl git make build-essential gnupg2
 cd ~/dotfiles
 curl -fLo ~/.local/share/nvim/site/pack/jetpack/opt/vim-jetpack/plugin/jetpack.vim --create-dirs https://raw.githubusercontent.com/tani/vim-jetpack/master/plugin/jetpack.vim
 
-curl -fsSL https://bun.sh/install | bash
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
-curl -fsSL https://deno.land/install.sh | sh
-
-gpg2 --keyserver keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-curl -sSL https://get.rvm.io | bash -s stable
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-curl -sSfL https://raw.githubusercontent.com/aquaproj/aqua-installer/v3.1.1/aqua-installer | bash
+curl https://mise.run | bash
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
 
 source ~/.bashrc
@@ -35,17 +28,9 @@ link ~/dotfiles/global-package.json ~/.bun/install/global/package.json
 link ~/dotfiles/.gitconfig ~/.gitconfig
 link ~/dotfiles/.gitignore_global ~/.gitignore_global
 link ~/dotfiles/starship.toml ~/.config/starship.toml
-link ~/dotfiles/aqua.yaml ~/aqua.yaml
 link ~/dotfiles/bat.config ~/.config/bat/config
 link ~/dotfiles/gh.config.yml ~/.config/gh/config.yml
-
-aqua install
-bun i -g pnpm
-nvm install --lts
-cargo binstall -y eza
-
-cargo install --git https://github.com/sevenc-nanashi/envcache.git
-cargo install --git https://github.com/sevenc-nanashi/ccsum.git
+link ~/dotfiles/mise ~/.config/mise
 
 git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git
 make -C ble.sh install PREFIX=~/.local
